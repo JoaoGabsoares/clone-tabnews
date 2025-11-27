62f9b8dbc497cd9b16ef527d0eb531920d139b39
joao <joao.dev.soares@gmail.com>
2025-11-27 15:55:44 -0300

    ajustes de tags html

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 export default function home() {
     return (
-        <><h1>
-            Thais eu te <strong>AMO</strong>
-        </h1><h2>
-                <p>
-                    mais um teste
-                </p>
-            </h2></>
+        <>
+            <h1>
+                Thais eu te <strong>AMO</strong>
+            </h1>
+            <p>
+                mais um teste
+            </p>
+        </>
     );
 }   
