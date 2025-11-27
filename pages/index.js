cef69c0fe9c6f01f0c17c56f8d1ded381146681f
joao <joao.dev.soares@gmail.com>
2025-11-27 13:40:30 -0300

    adição de texto

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function home() {
     return (
-        <h1>
+        <><h1>
             Thais eu te <strong>AMO</strong>
-        </h1>
+        </h1><h2>
+                <p>
+                    mais um teste
+                </p>
+            </h2></>
     );
 }  
