bff2b102c0cc3d784dadf7663c16332e0d9d36a7
joao <joao.dev.soares@gmail.com>
2025-12-08 20:03:44 -0300

    Adiciona `prettier`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
-export default function home() {
-    return (
-        <>
-            <h1>
-                A BELA DECLARAÇÃO DE AMOR
-            </h1>
-            <p>
-                Thais eu te <strong>amor</strong> muito
-            </p>
-        </>
-    );
-}   
+function Home() {
+  return <h1> Thais eu te amo</h1>;
+}
+
+export default Home;
